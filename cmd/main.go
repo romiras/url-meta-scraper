502d823fbb627e5e26c982d0dcd1088d56eca2f4
Romiras <298544+romiras@users.noreply.github.com>
2020-09-01 17:14:27 +0300

    use Registry for BL

--- cmd/main.go
+++ cmd/main.go
@@ -1,101 +1,17 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/romiras/url-meta-scraper/log"
 	"github.com/romiras/url-meta-scraper/registries"
-	"github.com/streadway/amqp"
 )
-
-/*
-const DefaultAmqpURI = "amqp://guest:guest@localhost:5672"
-
-func scanURLs(reg *registries.Registry, urlsRcv chan string, payloadsTx chan []byte, logger log.Logger) {
-	for url := range urlsRcv {
-		urlScraped, attempts, err := reg.FetchHelper.Try(reg.Fetcher, url, 0)
-		if err != nil {
-			if attempts == 0 {
-				GiveUp(err)
-			} else {
-				// Retry(reg, url, attempts)
-				logger.Info("Retry", attempts, url)
-				// urlsRcv <- url
-			}
-			continue
-		}
-
-		payload, err := json.Marshal(urlScraped)
-		if err != nil {
-			panic(err)
-		}
-
-		payloadsTx <- payload
-	}
-}
-
-func sender(reg *registries.Registry, payloadsRcv chan []byte) {
-	for payload := range payloadsRcv {
-		task, err := producers.NewTask(payload)
-		if err != nil {
-			reg.Logger.Error(err)
-		}
-
-		_, err = reg.ScrapedURLPublisher.Produce(task)
-		if err != nil {
-			reg.Logger.Error(err)
-		}
-	}
-}
-
-func Do(reg *registries.Registry) {
-	payloadsChan := make(chan []byte)
-
-	go func() {
-		sender(reg, payloadsChan)
-	}()
-
-	go func() {
-		scanURLs(reg, urlsChan, payloadsChan)
-		close(payloadsChan)
-	}()
-}
-
-func GiveUp(err error) {
-	logger.Info(err.Error())
-}
-
-func Retry(reg *registries.Registry, url string, attempts uint) {
-	logger.Info("\tRetry", attempts, url, "-> failed-urls")
-	// TODO: (URL, attempt nr.) -> queue 'failed-urls'
-	// err := reg.RedisPublisher.Publish("failed-urls", payload)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// Consider Exponential Backoff algorithm  https://blog.miguelgrinberg.com/post/how-to-retry-with-class
-}
-*/
-
-func handler(deliveries <-chan amqp.Delivery, logger log.Logger) {
-	multiAck := false
-	for msg := range deliveries {
-		logger.Info("-> msg")
-		fmt.Println(string(msg.Body))
-		err := msg.Ack(multiAck)
-		if err != nil {
-			logger.Error(err)
-		}
-	}
-	logger.Info("handle: deliveries channel closed / no new messages")
-}
 
 func main() {
 	reg := registries.NewRegistry()
 	defer reg.Close()
 
-	err := reg.URLSubscriber.Consume(handler, reg.Logger)
+	err := reg.URLSubscriber.Consume(reg.ConsumerHandler, reg.Logger)
 	if err != nil {
 		reg.Logger.Fatal(err)
+		return
 	}
 
 	reg.Logger.Info(" [*] Waiting for messages. To exit press CTRL+C")
--- registries/registry.go
+++ registries/registry.go
@@ -1,11 +1,15 @@
 package registries
 
 import (
+	"encoding/json"
+
 	"github.com/romiras/url-meta-scraper/consumers"
 	"github.com/romiras/url-meta-scraper/initializers"
 	"github.com/romiras/url-meta-scraper/log"
+	"github.com/romiras/url-meta-scraper/pkg"
 	"github.com/romiras/url-meta-scraper/producers"
 	"github.com/romiras/url-meta-scraper/services"
+	"github.com/streadway/amqp"
 )
 
 type Registry struct {
@@ -42,3 +46,91 @@ func (reg *Registry) Close() {
 		reg.Logger.Error(err)
 	}
 }
+
+func (reg *Registry) handleURL(url string) bool {
+	urlScraped, attempts, err := reg.FetchHelper.Try(reg.Fetcher, url, 0)
+	if err != nil {
+		if attempts == 0 {
+			reg.Logger.Info("Giving up", err.Error())
+		} else {
+			reg.Retry(url, attempts)
+		}
+		return false
+	}
+
+	err = reg.produceScrapedURLTask(urlScraped)
+	if err != nil {
+		reg.Logger.Error(err)
+		return false
+	}
+
+	return true
+}
+
+func (reg *Registry) produceScrapedURLTask(urlScraped *pkg.UrlScraped) error {
+	payload, err := json.Marshal(urlScraped)
+	if err != nil {
+		panic(err)
+	}
+
+	task, err := producers.NewTask(payload)
+	if err != nil {
+		return err
+	}
+
+	_, err = reg.ScrapedURLPublisher.Produce(task)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (reg *Registry) Retry(url string, attempts uint) {
+	reg.Logger.Info("\tRetry", attempts, url, " -> failed-urls")
+	// TODO: (URL, attempt nr.) -> queue 'failed-urls'
+	// err := reg.RedisPublisher.Publish("failed-urls", payload)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// Consider Exponential Backoff algorithm  https://blog.miguelgrinberg.com/post/how-to-retry-with-class
+}
+
+func (reg *Registry) ConsumerHandler(deliveries <-chan amqp.Delivery, logger log.Logger) {
+	multiAck := false
+	for msg := range deliveries {
+		logger.Info("-> msg")
+		reg.handleURL(string(msg.Body))
+
+		err := msg.Ack(multiAck)
+		if err != nil {
+			logger.Error(err)
+		}
+	}
+	logger.Info("handle: deliveries channel closed / no new messages")
+}
+
+/*
+func (reg *Registry) scanURLs(urlsRcv chan string, payloadsTx chan []byte) {
+	for url := range urlsRcv {
+		urlScraped, attempts, err := reg.FetchHelper.Try(reg.Fetcher, url, 0)
+		if err != nil {
+			if attempts == 0 {
+				reg.Logger.Info("Giving up", err.Error())
+			} else {
+				// Retry(reg, url, attempts)
+				reg.Logger.Info("Retry", attempts, url)
+				// urlsRcv <- url
+			}
+			continue
+		}
+
+		payload, err := json.Marshal(urlScraped)
+		if err != nil {
+			reg.Logger.Fatal(err)
+		}
+
+		payloadsTx <- payload
+	}
+}
+*/
