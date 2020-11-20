844a034ab4e2213cc34b944a2aef23b3c65f7b39
Romiras <298544+romiras@users.noreply.github.com>
2020-11-21 00:33:46 +0200

    del ScrapedEvent

--- pkg/events/scraped.go
+++
@@ -1,9 +0,0 @@
-package events
-
-import "encoding/json"
-
-type ScrapedEvent struct {
-	Body     []byte            `json:"body"`
-	Headers  map[string]string `json:"headers"`
-	Metadata json.RawMessage   `json:"metadata,omitempty"`
-}
